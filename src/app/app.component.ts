66196c964c26d080252e771f5c00b405f492374b
Michal Boczula <michal.boczula0101@gmail.com>
2023-01-15 19:59:26 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rx-training';
+
+  ngOnInit(): void {
+    of(1, 2, 3, 4).subscribe({
+      next: (item) => console.log(`... ${item}`),
+      error: (err) => console.error(`... ${err}`),
+      complete: () => console.log('rawrrr')
+    })
+    from([1, 2, 3, 4]).subscribe({
+      next: (item) => console.log(`... ${item}`),
+      error: (err) => console.error(`... ${err}`),
+      complete: () => console.log('rawrrr')
+    })
+  }
 }
