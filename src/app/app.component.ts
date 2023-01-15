3a9623366f98eef002ecaa0f25e6c2a2da6c028a
Michal Boczula <michal.boczula0101@gmail.com>
2023-01-15 20:15:08 +0100

    added example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs';
+import { from, of, map, tap, take, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,5 +20,23 @@ export class AppComponent implements OnInit {
       error: (err) => console.error(`... ${err}`),
       complete: () => console.log('rawrrr')
     })
+
+    from([1, 2, 3, 4, 5])
+      .pipe(
+        tap(ele => console.log(`orginal ${ele}`)),
+        map(ele => ele * 2),
+        map(ele => ele - 2),
+        // map(ele => {
+        //   if (ele === 0) {
+        //     throw new Error('zero!!!')
+        //   }
+        // }),
+        take(3)
+      )
+      .subscribe({
+        next: (item) => console.log(`result ${item}`),
+        error: (err) => console.error(`... ${err}`),
+        complete: () => console.log('rawrrr')
+      })
   }
 }
